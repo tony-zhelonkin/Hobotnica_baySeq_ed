070b68ea8d1a05f567b661a9ef54c44ef5cfaee1
mirachirkova <mira.chirkova@gmail.com>
2021-09-10 18:08:59 +0300

    voom diff expression analysis for one condition

--- voom.R
+++ voom.R
@@ -1,22 +1,3 @@
-#Find data
-sample_name <- "SRR1039"
-location <- paste("quants/", sample_name, sep = "", collapse = "")
-
-#Create data array
-names <- c()
-files <- c()
-conditions <- c()
-cell_line <- c()
-for (i in "508":"523") {
-    names <- c(names, paste(sample_name, i, sep = "", collapse = ""))
-    files <- c(files, paste(location, i, "_quant/quant.sf", sep = "", collapse = ""))
-}
-conditions_dexamethasone <- c(conditions, "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated", "Untreated", "Treated")
-conditions_albuterol <- c(conditions, "Untreated", "Untreated", "Treated", "Treated", "Untreated", "Untreated", "Treated", "Treated", "Untreated", "Untreated", "Treated", "Treated", "Untreated", "Untreated", "Treated", "Treated")
-cell_line <- c(cell_line, "N61311", "N61311", "N61311", "N61311", "N052611", "N052611", "N052611", "N052611", "N080611", "N080611", "N080611", "N080611", "N061011", "N061011", "N061011", "N061011")
-coldata <- data.frame(names = names, files = files, conditions_dexamethasone = conditions_dexamethasone, conditions_albuterol = conditions_albuterol, cell_line = cell_line, stringsAsFactors=FALSE)
-coldata
-
 
 #use tximeta
 library("BiocManager")
@@ -24,21 +5,23 @@ library("dplyr")
 library("tximeta")
 library("SummarizedExperiment")
 
-gse <- readRDS(file = "matrix.rds")
-
+gse <- readRDS(file = "data/matrix.rds")
+coldata <- readRDS(file = "data/coldata.rds")
 library("edgeR")
 library("limma")
+
+condition <- coldata$condition
 
 d <- makeDGEList(gse)
 
 #calculate diff expression
-design <- model.matrix(~conditions_dexamethasone + 0, data = coldata)
+design <- model.matrix(~condition + 0, data = coldata)
 y <- voom(d, design)
 cat ("VOOMED\n")
 fit <- lmFit(y, design)
 head(coef(fit))
 
-contrasts <- makeContrasts(conditions_dexamethasoneUntreated - conditions_dexamethasoneTreated, levels = colnames(coef(fit)))
+contrasts <- makeContrasts(conditionUntreated - conditionTreated, levels = colnames(coef(fit)))
 contrasts
 fit2 <- contrasts.fit(fit, contrasts)
 fit2 <- eBayes(fit2, trend=TRUE)
