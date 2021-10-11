531ff4c0b0005e9768a5568e7b6026da1fa75d28
omashkova <mashkova@ispras.ru>
2021-10-11 17:31:03 +0300

    filter genes by logFC and p-value

---
+++ filtered_voom.R
@@ -0,0 +1,10 @@
+voom_filtered <- function(filename) {
+  library("edgeR")
+  # reading results of diff. expression analysis from RDS file
+  results <- readRDS(file = filename)
+  
+  # filtering results by log2FC >= 2 and p-value < 0.05
+  filtered_results <- results[abs(results$logFC) >= 2 && results$P.Value < 0.05, ]
+  
+  return(filtered_results)
+}
