d5b4570413dfe45dbf25e7936c0477d482953213
omashkova <mashkova@ispras.ru>
2021-10-11 18:08:42 +0300

    filter genes by logFC and p-value

---
+++ filtered_EBSeq.R
@@ -0,0 +1,14 @@
+ebseq_filtered <- function(filename) {
+  library("EBSeq")
+  
+  # reading results of diff. expression analysis from RDS file
+  results <- readRDS(file = filename)
+  
+  # filtering results by PPDE >= 0.95
+  ppde <- results$PPDE
+  ppde <- ppde[ppde >= 0.95]
+  ppde <- as.data.frame(ppde)
+  colnames(ppde) <- c("PPDE")
+  res <- gsub("\\..*","",row.names(ppde))
+  return(res)
+}
