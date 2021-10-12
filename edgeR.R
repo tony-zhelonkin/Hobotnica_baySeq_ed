c86827997899a0ec6a50328c18307c2a9123a617
omashkova <mashkova@ispras.ru>
2021-10-12 15:11:35 +0300

    added gene filter by logFC and p-value

--- edgeR.R
+++ edgeR.R
@@ -56,19 +56,31 @@ edger_v <- function(edger_res) {
             subtitle = "Differential expression")
 }
 
-# Make a signature of top-20 genes
-edger_top <- function(results) {
+# Make a signature of top-n genes
+edger_top <- function(results, n) {
     library("edgeR")
     library("biomaRt")
 
     # Filter results by logFC > 1 or logFC < -1
     filtered_results <- results[abs(results$logFC) > 1, ]
 
-    # Extract top-20 differentially expressed genes ordered by p-value
-    top <- head(filtered_results[order(filtered_results$PValue), ], 20)
+    # Extract top-n differentially expressed genes ordered by p-value
+    top <- head(filtered_results[order(filtered_results$PValue), ], n)
     tmp <- gsub("\\..*","",row.names(top))
 
-    # Write top-20 genes with original (ENSEMBL) encoding
+    # Write top-n genes with original (ENSEMBL) encoding
     return(tmp)
 
+}
+
+# Filter genes by logFC and p-value
+edger_filtered <- function(filename) {
+    library("edgeR")
+    # reading results of diff. expression analysis from RDS file
+    results <- readRDS(file = filename)
+    
+    # filtering results by log2FC >= 2 and p-value < 0.05
+    filtered_results <- results[abs(results$logFC) >= 2 && results$PValue < 0.05, ]
+    filtered_genes <- gsub("\\..*","",row.names(filtered_results))
+    return(filtered_genes)
 }
